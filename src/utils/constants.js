0af6143029e6504523819303ebf87c6eba1746de
thesumitsuryawanshi <thesumit0013@gmail.com>
2024-04-17 12:01:01 +0530

    Icons changes done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,12 +11,12 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
-
+import PetsIcon from "@mui/icons-material/Pets";
 export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
+  { name: "Mr Beast", icon: <PetsIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
   { name: "NextJS", icon: <CodeIcon /> },
